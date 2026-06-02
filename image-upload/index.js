5dfe008cb66cc393f2c53472cff643c3ef682fd1
Vikky Verma <Vikkyverma7054@gmail.com>
2026-06-02 16:41:26 +0530

    multer libraries

---
+++ image-upload/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const multer = require('multer');
+
+
+const app = express();
+const PORT = 8000;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const upload = multer({ storage });
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
+app.use(express.urlencoded({ extended: false })); // to parse form data
+
+
+app.get('/', (req, res) => {
+  return res.render('homepage');
+});
+
+app.post('/upload', upload.single('ProfileImage'), (req, res) => {
+  console.log(req.body);
+  console.log(req.file); // file information
+
+  return res.redirect('/');
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
